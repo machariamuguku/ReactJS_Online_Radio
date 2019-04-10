4f40a9df5dc2c5eef32eaa4642c0aa6ab3babeb2
Muguku <machariamuguku@gmail.com>
2019-04-10 11:23:34 +0300

    changed to specific file player for type audio

--- src/Radio.js
+++ src/Radio.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
-import ReactPlayer from 'react-player'
+import FilePlayer from 'react-player/lib/players/FilePlayer'
+
 import play from './resources/play.png'
 import pause from './resources/pause.png'
-
 class RadioPlayer extends Component {
 
     constructor(props) {
@@ -37,28 +37,30 @@ class RadioPlayer extends Component {
 
     render() {
         const { radioUrl, playing, volume, muted } = this.state
-       
-            return (
-                <div>
-                    <label htmlFor='volume'>volume</label>
-                    <input type='range' min={0} max={1} step='any' value={volume} onChange={this.setVolume} />
-                    <label htmlFor='muted'>Muted</label>
-                    <input id='muted' type='checkbox' checked={muted} onChange={this.toggleMuted} />
-                    <h3> {playing ? 'HBR 103.5 is Playing!!!' : 'HBR 103.5 is Paused!!!'} </h3>
-                    <button onClick={this.playPause}> <img src={playing ? pause : play} alt="pause" /> </button>
-                    <ReactPlayer
-                        url={radioUrl}
-                        playing={playing}
-                        volume={volume}
-                        muted={muted}
-                        width='1%'
-                        height='1%'
-                        onPlay={this.onPlay}
-                        onPause={this.onPause}
-                        onError={e => console.log('onError', e)}
-                    />
-                </div>
-            )
+
+        return (
+            <div>
+                <label htmlFor='volume'>volume</label>
+                <input type='range' min={0} max={1} step='any' value={volume} onChange={this.setVolume} />
+                <label>{(volume * 10).toFixed(1)}</label>
+                <label htmlFor='muted'>Muted</label>
+                <input id='muted' type='checkbox' checked={muted} onChange={this.toggleMuted} />
+                <h3> {playing ? 'HBR 103.5 is Playing!!!' : 'HBR 103.5 is Paused!!!'} </h3>
+                <button onClick={this.playPause}> <img src={playing ? pause : play} alt="pause" /> </button>
+                <FilePlayer
+                    fileConfig={{ forceAudio: true }}
+                    url={radioUrl}
+                    playing={playing}
+                    volume={volume}
+                    muted={muted}
+                    width='0'
+                    height='0'
+                    onPlay={this.onPlay}
+                    onPause={this.onPause}
+                    onError={e => console.log('onError', e)}
+                />
+            </div>
+        )
     }
 }
 
